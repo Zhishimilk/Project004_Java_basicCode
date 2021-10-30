f5efac709f191ebc43cc693712833f6bb5acd4b7
Zhishimilk <zhishimilk@qq.com>
2021-10-30 18:49:34 +0800

    Code of today's(20211030) exercise

--- Day-20211027/src/com/bilibili/workCode/RunnableClass.java
+++ Day-20211027/src/com/bilibili/workCode/RunnableClass.java
@@ -1,27 +1,71 @@
 package com.bilibili.workCode;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class RunnableClass implements Runnable{
 
-    private int ticket = 100;
-    Object o = new Object();
+    private static int ticket = 100;
+
+    Lock l = new ReentrantLock();
     @Override
     public void run() {
 
-        //添加锁对象于同步代码块
-        while( ticket > 0) {
-            synchronized(o) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-//                try {
-//                    Thread.sleep(20);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-                if(ticket > 0) {
+        l.lock();
+        while (ticket > 0) {
+
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName());
+                System.out.println("ticket of " + ticket);
+                ticket--;
+            }
+        }
+        l.unlock();
+//        sync();
+//        syncStc();
+    }
+
+    //同步方法
+    public /*synchronized*/ void sync(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //添加锁对象于同步代码块
+        synchronized (this) {
+            while (ticket > 0) {
+
+                if (ticket > 0) {
                     System.out.println(Thread.currentThread().getName());
                     System.out.println("ticket of " + ticket);
                     ticket--;
                 }
             }
         }
+    }
+    //
+    public static synchronized void syncStc(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        synchronized (RunnableClass.class){
+            while (ticket > 0) {
+
+                if (ticket > 0) {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("ticket of " + ticket);
+                    ticket--;
+                }
+            }
+//        }
     }
 }
